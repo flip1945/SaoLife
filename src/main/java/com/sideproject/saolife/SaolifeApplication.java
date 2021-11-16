bc93b502a59dd57214800ef3375ce4e36a7c797e
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-11-16 16:18:37 +0900

    initial commit

---
+++ src/main/java/com/sideproject/saolife/SaolifeApplication.java
@@ -0,0 +1,13 @@
+package com.sideproject.saolife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaolifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaolifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sideproject/saolife/SaolifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sideproject.saolife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaolifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
