4ac774eb500c472a2d033333e2196e5d676b5050
flip1945 <77256607+flip1945@users.noreply.github.com>
2021-11-29 22:19:15 +0900

    test: Post 생성, 수정 시간 테스트 추가

--- src/test/java/com/sideproject/saolife/posts/service/PostServiceTest.java
+++ src/test/java/com/sideproject/saolife/posts/service/PostServiceTest.java
@@ -4,15 +4,21 @@ import com.sideproject.saolife.member.domain.Member;
 import com.sideproject.saolife.member.service.MemberService;
 import com.sideproject.saolife.posts.domain.Post;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import java.time.LocalDateTime;
 
 import static org.assertj.core.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Transactional
 public class PostServiceTest {
 
     @Autowired
@@ -20,6 +26,9 @@ public class PostServiceTest {
 
     @Autowired
     private MemberService memberService;
+
+    @Autowired
+    private EntityManager em;
 
     @Test
     public void 게시글_저장() {
@@ -45,4 +54,58 @@ public class PostServiceTest {
         assertThat(findPost.getContent()).isEqualTo(post.getContent());
     }
 
+    @Test
+    public void 게시글_생성시간_확인() {
+        // given
+        LocalDateTime now = LocalDateTime.now();
+
+        Member member = Member.builder()
+                .email("coghdus1016@naver.com")
+                .password("1234")
+                .build();
+
+        Post post = Post.builder()
+                .title("hi")
+                .content("hihihihi")
+                .member(member)
+                .build();
+
+        memberService.join(member);
+        Long postId = postService.save(post);
+
+        // when
+        Post findPost = postService.findOne(postId).get();
+
+        // then
+        Assertions.assertTrue(findPost.getCreatedDate().isAfter(now));
+    }
+
+    @Test
+    public void 게시글_수정시간_확인() {
+        // given
+        Member member = Member.builder()
+                .email("coghdus1016@naver.com")
+                .password("1234")
+                .build();
+
+        Post post = Post.builder()
+                .title("hi")
+                .content("hihihihi")
+                .member(member)
+                .build();
+
+        memberService.join(member);
+        Long postId = postService.save(post);
+
+        // when
+        LocalDateTime now = LocalDateTime.now();
+        Post findPost = postService.findOne(postId).get();
+
+        postService.updatePost(findPost.getId(), "new title", "modified content");
+
+        em.flush();
+
+        // then
+        Assertions.assertTrue(findPost.getModifiedDate().isAfter(now));
+    }
 }
